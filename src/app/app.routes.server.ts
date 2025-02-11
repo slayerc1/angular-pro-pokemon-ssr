f39964b30978311a3abdd4cc2b8a06a1de2faf13
TourBandLive <tourbandlive@gmail.com>
2025-02-11 22:28:43 +0100

    Add getPrerenderParams

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { RenderMode, PrerenderFallback, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemons/:id',
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Client,
+    async getPrerenderParams() {
+      return [{ id: '1' }, { id: '2' }, { id: '3' }];
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
